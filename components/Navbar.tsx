76d9037c6770b5eb51761b393079c9aeab73c868
khurrams4 <khurram.shahzad@itcurves.com>
2023-10-11 16:41:59 +0500

    finished with navbar...

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
+const navIcons=[
+  {src:'/assets/icons/search.svg' , alt:'search'},
+  {src:'/assets/icons/black-heart.svg' , alt:'heart'},
+  {src:'/assets/icons/user.svg' , alt:'user'},
+]
 const Navbar = () => {
   return (
     <header className='w-full'>
@@ -13,7 +18,13 @@ const Navbar = () => {
 
              </p>
             </Link>
-            <div>Navbar</div>
+            <div className='flex items-center gap-5'>
+            {
+              navIcons.map((icon)=>(
+              <Image key={icon.alt}src={icon.src} alt={icon.alt} width={28} height={28} className='object-contain'/>
+              ))
+            }
+            </div>
 
         </nav>
     </header>
