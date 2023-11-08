1a78240f8f7b6f52e2130f7969fa1630bee744e3
khurrams4 <khurram.shahzad@itcurves.com>
2023-11-08 20:14:28 +0500

    inDetails...

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//       domains: ['lh3.googleusercontent.com', 'res.cloudinary.com', 'task.com']
+//     },
+//     experimental: {
+//       serverActions:true,
+//       serverComponentsExternalPackages:['mongoose'],
+//       serverComponentsExternalPackages: ['cloudinary', 'graphql-request'],
+//     },
+//     eslint: {
+//       // Warning: This allows production builds to successfully complete even if
+//       // your project has ESLint errors.
+//       ignoreDuringBuilds: true
+//     }
+//   }
+  
+//   module.exports = nextConfig
+
+
+  /** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ['mongoose']
+  },
+  images: {
+    domains: ['m.media-amazon.com']
+  }
+}
 
 module.exports = nextConfig
